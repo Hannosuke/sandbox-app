9923f527eeb536f55fe4d7b3ee954ee6decb8d4d
ryuhanda <r.handa@everyleaf.com>
2024-03-28 22:55:11 +0900

    updateアクションを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[edit update]
+
   def index
     @tasks = Task.all
   end
@@ -18,12 +20,23 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def task_params
     params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
